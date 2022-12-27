5888748faccdef8730de9f084f45c1fe9c426721
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 00:38:55 +0300

    Add files via upload

--- stage1.sh
+++ stage1.sh
@@ -24,9 +24,6 @@ rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-5.noarch.rpm
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
 yum --enablerepo=mysql80-community install mysql-community-server
 
-\cp -u /root/Project_Otus/config/my.cnf /etc/
-chmod -R 755 /var/lib/mysql/
-
 systemctl enable --now mysqld
 
 sleep 10
@@ -42,18 +39,18 @@ set timeout 10
 spawn mysql_secure_installation
 expect "Enter password for user root:"
 send "$MYSQL\r"
-expect "Change the password for root ? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
 expect "New password:"
 send "$Pass\r"
 expect "Re-enter new password:"
 send "$Pass\r"
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "n\r"
 expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
 send "y\r"
 expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
 expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
+send "n\r"
 expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
 expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
@@ -63,4 +60,8 @@ expect eof
 
 echo "$SECURE_MYSQL"
 
+\cp -u /root/Project_Otus/config/my.cnf /etc/
+chmod -R 755 /var/lib/mysql/
+
 systemctl restart mysqld
+
