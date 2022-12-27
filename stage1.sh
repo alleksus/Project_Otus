4ef9b46369c151453280cc89fae36ccc8531f488
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 00:18:57 +0300

    Add files via upload

--- stage1.sh
+++ stage1.sh
@@ -38,34 +38,24 @@ systemctl status mysqld
 MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
 
 SECURE_MYSQL=$(expect -c "
-
 set timeout 10
 spawn mysql_secure_installation
-
 expect "Enter password for user root:"
 send "$MYSQL\r"
-
-expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+expect "Change the password for root ? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
-
 expect "New password:"
 send "$Pass\r"
-
 expect "Re-enter new password:"
 send "$Pass\r"
-
 expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
 send "y\r"
-
 expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
-
 expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
-
 expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
-
 expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
 send "y\r"
 expect eof
