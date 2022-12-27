d9fd66367ab220dfe839dfe8d0f18f4e77040313
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 00:14:09 +0300

    Add files via upload

--- stage1.sh
+++ stage1.sh
@@ -26,7 +26,6 @@ yum --enablerepo=mysql80-community install mysql-community-server
 
 \cp -u /root/Project_Otus/config/my.cnf /etc/
 chmod -R 755 /var/lib/mysql/
-chmod 777 /var/run/mysqld/mysqld.sock
 
 systemctl enable --now mysqld
 
@@ -36,30 +35,45 @@ systemctl status mysqld
 
 #настройка
 
-root_temp_pass=$(grep "A temporary password" /var/log/mysqld.log)
-echo "root_temp_pass: "$root_temp_pass
+MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
+
 SECURE_MYSQL=$(expect -c "
-set timeout 1
+
+set timeout 10
 spawn mysql_secure_installation
-expect \"Enter password for user root:\"
-send \"$root_temp_pass\r\"
-expect \"New password:\"
-send \"$Pass\r\"
-expect \"Re-enter new password:\"
-send \"$Pass\"
-expect \"Change the root password?\"
-send \"n\r\"
-expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"y\r\"
-expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
+
+expect "Enter password for user root:"
+send "$MYSQL\r"
+
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "New password:"
+send "$Pass\r"
+
+expect "Re-enter new password:"
+send "$Pass\r"
+
+expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
 expect eof
 ")
 
+echo "$SECURE_MYSQL"
+
+echo "$SECURE_MYSQL"
 echo "$SECURE_MYSQL"
 
 systemctl restart mysqld
