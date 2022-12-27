3547424815b36e798a84516b19a7b17b3f7cbe65
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-27 23:09:47 +0300

    Add files via upload

--- stage1.sh
+++ stage1.sh
@@ -7,57 +7,26 @@ DUMP="/tmp/$DB-export.sql"
 Master_Host=192.168.136.7
 Slave_Host=192.168.136.8
 
-# настройка firewall
-firewall-cmd --permanent --add-service=http  
-firewall-cmd --permanent --add-service=https 
-firewall-cmd --permanent --add-port=8080/tcp --add-port=8081/tcp --add-port=8082/tcp --add-port=3306/tcp --add-port=9090/tcp --add-port=9100/tcp --add-port=9200/tcp --add-port=5601/tcp 
-systemctl restart firewalld
-
-# установка доп ПО
-setenforce 0
-yum install -y yum-utils rpm wget tar nano mc git expect sshpass
-
-# клонирование репозитория
-git clone git@github.com:alleksus/Project_Otus.git
-
-rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-5.noarch.rpm
-sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
-yum --enablerepo=mysql80-community install mysql-community-server
-
-sleep 5
-
-systemctl start mysqld
-systemctl enable mysqld
-
-sleep 5
-
-systemctl status mysqld
-
-#настройка
-
 root_temp_pass=$(grep "A temporary password" /var/log/mysqld.log)
 
 echo "root_temp_pass: "$root_temp_pass
-
-SECURE_MYSQL=$(expect -c "
+secure_mysql=$(expect -c "
 set timeout 1
-spawn mysql_secure_installation
-expect \"Enter password for user root:\"
-send \"$root_temp_pass\r\"
+spawn mysql_secure_installation "-u$User" "-p#$root_temp_pass"
 expect \"New password:\"
-send \"$Pass\r\"
+send \"$Pass\"
 expect \"Re-enter new password:\"
 send \"$Pass\"
 expect \"Change the root password?\"
-send \"n\r\"
+send \"n\"
 expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"y\r\"
+send \"y\"
+expect \"Disable root login remotely?\"
+send \"n\"
 expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
+send \"y\"
+expect \"Remove privilege tables now?\"
+send \"y\"
 expect eof
 ")
 
