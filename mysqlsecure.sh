3adce92731ac9759809b3249aa593d1e1463ae1a
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 01:44:12 +0300

    Add files via upload

--- mysqlsecure.sh
+++ mysqlsecure.sh
@@ -9,6 +9,8 @@ Slave_Host=192.168.136.8
 
 MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
 
+spawn  mysql_secure_installation
+
 expect {
              "Enter password for user root:" { send "$MYSQL\r"; exp_continue }
              "New password:" { send "$Pass\r"; exp_continue }
