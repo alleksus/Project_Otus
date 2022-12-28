8be18563cce48cdc8b79fb03abf54b89c428f35a
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 16:51:51 +0300

    Update stage1.sh

--- stage1.sh
+++ stage1.sh
@@ -5,7 +5,7 @@ Pass=Otus_2022
 
 Master_Status=$(mysql "-u$User" "-p$Pass" -ANe "SHOW MASTER STATUS;" | awk '{print $1 " " $2}')
 
-cat <<EOF : tee /tmp/binlog.txt
+cat <<EOF | tee /tmp/binlog.txt
 $Master_Status
 EOF
 
