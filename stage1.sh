b9ddcd4338f0bda66e39aee932dc9f693dcc31aa
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 13:14:33 +0300

    Add files via upload

--- stage1.sh
+++ stage1.sh
@@ -3,7 +3,7 @@
 User=root
 Pass=Otus_2022
 MYSQL=/usr/bin/mysql
-DUMP="/tmp/mysql_dump.sql"
+DUMP="/tmp/$DB_dump.sql"
 Master_Host=192.168.136.7
 Slave_Host=192.168.136.8
 
@@ -12,8 +12,10 @@ mysql "-u$User" "-p$Pass" -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';"
 mysql "-u$User" "-p$Pass" -e "CREATE DATABASE Otus;"
 
 mysql "-u$User" root "-p$Pass" -e "STOP SLAVE;"
-mysqldump "-u$User" "-p$Pass" --events --routines --all-databases --master-data=2 > $DUMP
 
+for DB in $(mysql -e 'show databases' -s --skip-column-names); do
+    mysqldump $DB > $DUMP;
+done
 
 Master_Status=$(mysql "-u$User" "-p$Pass" -ANe "SHOW MASTER STATUS;" | awk '{print $1 " " $2}')
 Log_File=$(echo $Master_Status |cut -f1 -d ' ')
