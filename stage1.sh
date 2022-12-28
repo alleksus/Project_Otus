5aae3ae9a79f52141ba2e6991ac6241090538c85
alleksus <120936004+alleksus@users.noreply.github.com>
2022-12-28 10:35:45 +0300

    Add files via upload

--- stage1.sh
+++ stage1.sh
@@ -20,9 +20,9 @@ yum install -y yum-utils rpm wget tar nano mc git expect sshpass
 # клонирование репозитория
 git clone git@github.com:alleksus/Project_Otus.git
 
-wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm 
-yum localinstall mysql57-community-release-el7-11.noarch.rpm
-yum install mysql-community-server
+rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-5.noarch.rpm
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
+yum --enablerepo=mysql80-community install mysql-community-server
 
 systemctl start mysqld
 systemctl enable mysqld
@@ -35,31 +35,13 @@ systemctl status mysqld
 
 MYSQL=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
 
-SECURE_MYSQL=$(expect -c "
-set timeout 10
-spawn mysql_secure_installation
-expect "Enter password for user root:"
-send "$MYSQL\r"
-expect "New password:"
-send "$Pass\r"
-expect "Re-enter new password:"
-send "$Pass\r"
-expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
-send "n\r"
-expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
-send "y\r"
-expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "n\r"
-expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-expect eof
-")
-
-echo "$SECURE_MYSQL"
+mysql --user=root --password=${MYSQL} << EOFMYSQLSECURE
+UPDATE mysql.user SET Password = PASSWORD('Pass') WHERE User = 'root';
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+EOFMYSQLSECURE
 
 \cp -u /root/Project_Otus/config/my.cnf /etc/
 chmod -R 755 /var/lib/mysql/
